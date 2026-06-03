a188da8927167ead420dd447fd0c904c38a75a56
mari <souzadeaguiar.ma@gmail.com>
2026-06-03 16:08:46 -0300

    tabuleiro do termo, está errado não funciona

--- ignore/termo _física.py
+++ ignore/termo _física.py
@@ -5,6 +5,30 @@ palavras = [("fusão"), ("átomo"), ("quark"), ("força")]
 chute = ""
 tentativas = 6
 
+def tabuleiro():
+    topo = [" ___________"]
+    linha1 = ["| _ _ _ _ _ |"]
+    linha2 = ["| _ _ _ _ _ |"]
+    linha3 = ["| _ _ _ _ _ |"]
+    linha4 = ["| _ _ _ _ _ |"]
+    linha5 = ["| _ _ _ _ _ |"]
+    linha6 = ["| _ _ _ _ _ |"]
+    linha7 = ["|___________|"]
+    if tentativas == 6:
+        linha1[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4] 
+    elif tentativas == 5:
+        linha2[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4]
+    elif tentativas == 4:
+        linha3[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4]
+    elif tentativas == 3:
+        linha4[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4]
+    elif tentativas == 2:
+        linha5[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4]
+    else: 
+        tentativas == 1
+        linha6[2,4,6,8,10] = chute[0], chute[1], chute[2], chute[3], chute[4]
+    return "\n".join(topo + linha1 + linha2 + linha3 + linha4 + linha5 + linha6 + linha7)
+
 def verificar_chute(chute, palavra):
     resultado = []
     for i in range(len(chute)):
@@ -31,8 +55,10 @@ with open("ignore/regras_do_termo.txt", "r") as r:
 
 palavra = randomizar_palavra()
 
+print("\nDigite uma palavra de 5:")
 while True:
-    chute = input("\n aureoDigite uma palavra de 5 letras: ")
+    chute = input("")
+    exec(tabuleiro())
     if chute == palavra and tentativas <= 6:
         print("Parabéns! Você acertou a palavra!")
         break
