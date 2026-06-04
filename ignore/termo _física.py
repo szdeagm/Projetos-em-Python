39a860434d501f55793a1f54da98fa5ca0c3e9e8
mari <souzadeaguiar.ma@gmail.com>
2026-06-03 22:26:14 -0300

    mecanismo funcionando

--- ignore/termo _física.py
+++ ignore/termo _física.py
@@ -1,55 +1,29 @@
 import colorama
 from colorama import Fore, Style
+import getpass
 
 palavras = [("fusão"), ("átomo"), ("quark"), ("força")]
 chute = ""
 tentativas = 6
-linha1 = []
-linha2 = []
-linha3 = []
-linha4 = []
-linha5 = []
-linha6 = []
-
-
-def tabuleiro():
-    topo = [" ___________"]
-    linha1 = ["| _ _ _ _ _ |"]
-    linha2 = ["| _ _ _ _ _ |"]
-    linha3 = ["| _ _ _ _ _ |"]
-    linha4 = ["| _ _ _ _ _ |"]
-    linha5 = ["| _ _ _ _ _ |"]
-    linha6 = ["| _ _ _ _ _ |"]
-    linha7 = ["|___________|"]
-    if tentativas == 6:
-        linha1 = [range(len(chute))]   
-    elif tentativas == 5:
-        linha2 = [range(len(chute))]
-    elif tentativas == 4:
-        linha3 = [range(len(chute))]
-    elif tentativas == 3:
-        linha4 = [range(len(chute))]
-    elif tentativas == 2:
-        linha5 = [range(len(chute))]
-    elif tentativas == 1:
-        linha6 = [range(len(chute))]    
-    else: 
-        linha7 = [range(len(chute))]
-    return topo + linha1 + linha2 + linha3 + linha4 + linha5 + linha6 + linha7
+linha1 = ["-"]*5
+linha2 = ["-"]*5
+linha3 = ["-"]*5
+linha4 = ["-"]*5
+linha5 = ["-"]*5
+linha6 = ["-"]*5
 
 def verificar_chute(chute, palavra):
     resultado = []
     for i in range(len(chute)):
         if chute[i] == palavra[i]:
             resultado.append(Fore.GREEN + chute[i] + Style.RESET_ALL)
-            print(Fore.GREEN + chute[i] + Style.RESET_ALL, end="")
+
         elif chute[i] in palavra:
             resultado.append(Fore.YELLOW + chute[i] + Style.RESET_ALL)
-            print(Fore.YELLOW + chute[i] + Style.RESET_ALL, end="")
-        else:
-            resultado.append(chute[i])
-            print(Fore.RED + chute[i] + Style.RESET_ALL, end="")
 
+        else:
+            resultado.append(Fore.RED + chute[i] + Style.RESET_ALL)
+    
     return resultado
 
 def randomizar_palavra():
@@ -57,22 +31,56 @@ def randomizar_palavra():
     palavra = random.choice(palavras)
     return palavra
 
+def tabuleiro():
+    linhas = [linha1, linha2, linha3, linha4, linha5, linha6]
+    for linha in linhas:
+        print(" ".join(linha))
+
 print("Bem vindo ao TERMO-FÍSICO!")
 with open("ignore/regras_do_termo.txt", "r") as r:
     print(r.read())
 
 palavra = randomizar_palavra()
 
-print("\nDigite uma palavra de 5:")
 while True:
-    chute = input("")
-    exec(tabuleiro())
+    chute = input("\n Digite uma palavra de 5 letras:")
+    print("\n")
     if chute == palavra and tentativas <= 6:
-        print("Parabéns! Você acertou a palavra!")
+        linha_colorida = verificar_chute(chute, palavra)
+        if tentativas == 6:
+            linha1 = linha_colorida
+        elif tentativas == 5:
+            linha2 = linha_colorida
+        elif tentativas == 4:           
+            linha3 = linha_colorida
+        elif tentativas == 3:
+            linha4 = linha_colorida
+        elif tentativas == 2:
+            linha5 = linha_colorida
+        elif tentativas == 1:
+            linha6 = linha_colorida
+        tabuleiro()
+        print("\n Parabéns! Você acertou a palavra!")
         break
-    elif chute != palavra and tentativas <= 6 and tentativas > 0:
-        verificar_chute(chute, palavra)
+    
+    elif chute != palavra and tentativas > 0:
+        linha_colorida = verificar_chute(chute, palavra)
+        if tentativas == 6:
+            linha1 = linha_colorida
+        elif tentativas == 5:
+            linha2 = linha_colorida
+        elif tentativas == 4:           
+            linha3 = linha_colorida
+        elif tentativas == 3:
+            linha4 = linha_colorida
+        elif tentativas == 2:
+            linha5 = linha_colorida
+        elif tentativas == 1:
+            linha6 = linha_colorida
+        tabuleiro()
+        print("\n Você errou! Tente novamente.")
         tentativas -= 1
+
     else:
         print("Suas tentativas acabaram! A palavra era:", palavra)
         break
